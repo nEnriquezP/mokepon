a6429963120b58e0cad107e1161f43832cb9ead7
nEnriquezP <nenriquezp@miumg.edu.gt>
2024-06-28 12:21:51 -0600

    Añadiendo la lógica para leer a que mascota se elige

--- js/mokepongame.js
+++ js/mokepongame.js
@@ -1,11 +1,31 @@
-function seleccionarMascotaJugador(){
-    alert("Seleccionaste tu mascota")
-}
-
+function iniciarjuego(){
 let botonMascotaJugador = document.getElementById('boton-mascota')
 //let botonMascotaEnemigo
 
 botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+}
+
+function seleccionarMascotaJugador(){
+    
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+
+    if(inputHipodoge.checked) {
+        alert("Seleccionaste a Hipodoge")
+        }
+        else if(inputCapipepo.checked) {
+            alert("Seleccionaste a Capipepo")
+        }
+        else if(inputRatigueya.checked){
+            alert("Seleccionaste a Ratigueya")
+        }
+        else {
+            alert("Debes de Seleccionar algo")
+        }
+    
+}
 
 
+window.addEventListener('load',iniciarjuego)
 
