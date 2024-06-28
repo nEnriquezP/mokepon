e6655894618f92ddbaba6850e3e8e7a61a4a1446
nEnriquezP <nenriquezp@miumg.edu.gt>
2024-06-28 10:03:54 -0600

    Agregando ruta para el archivo .js + insertando el bloque del script en la parte de abajo

---
+++ js/mokepongame.js
@@ -0,0 +1,11 @@
+function seleccionarMascotaJugador(){
+    alert("Seleccionaste tu mascota")
+}
+
+let botonMascotaJugador = document.getElementById('boton-mascota')
+//let botonMascotaEnemigo
+
+botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+
+
